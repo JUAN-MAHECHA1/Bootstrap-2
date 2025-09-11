0b563d7fa6a68612bf17c0610596a269ff2112b8
Juan Mahecha <mahechajuanda29@gmail.com>
2025-09-11 16:08:23 -0500

    termine la section materials, termine la section  reviews

--- js/main.js
+++ js/main.js
@@ -33,4 +33,39 @@ $(document).ready(function () {
             }
         ]
     });
+
+    $('.slider-reviews').slick({
+        dots: false,
+        infinite: true,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 });
